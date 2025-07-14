796d27413e0e4f03c252811b0bf56c8658a10341
Apprivoisé <221840039@smail.nju.edu.cn>
2025-07-14 08:25:03 +0800

    chore: change title

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: "请记得 :)",
+  title: "WWWeeds' blog",
   /** Will be used in index page & copyright declaration */
   author: 'WWWeeds',
   /** Description metadata for your website. Can be used in page metadata. */
